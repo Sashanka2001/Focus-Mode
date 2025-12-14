907424bab04576ecd80bd2282bc9fc8f32a19795
Sashanka2001 <chamudisashanka01@gmail.com>
2025-12-14 22:21:12 +0530

    fix: correct import statement for Header component in App

--- focus-mode/src/App.jsx
+++ focus-mode/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
+
+import FocusSession from "./components/FocusSession";
 
 import AmbientSoundPanel from "./components/AmbientSoundPanel";
 
