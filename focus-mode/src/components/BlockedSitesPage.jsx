214478ff876560b3e3a0949fb40082d4712c95bd
Sashanka2001 <chamudisashanka01@gmail.com>
2025-12-15 11:40:22 +0530

    fix: adjust left margin in BlockedSitesPage component for improved layout

--- focus-mode/src/components/BlockedSitesPage.jsx
+++ focus-mode/src/components/BlockedSitesPage.jsx
@@ -3,7 +3,7 @@ import SiteList from './SiteList';
 
 const BlockedSitesPage = () => {
   return (
-    <div className="ml-64 p-6">
+    <div className="ml-6 p-6">
        <div className="w-full max-w-5xl">
       <h2 className="text-2xl font-bold mb-4">Blocked Sites</h2>
       </div>
